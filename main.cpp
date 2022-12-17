b257bf66903a180717f245cccac4e22040dae8d0
Gianluchin0 <118599894+Gianluchin0@users.noreply.github.com>
2022-12-17 03:28:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    char a[10][20];
+    char b[20];
+    int pos = 0;
+        for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 20; j++) {
+            a[i][j] = ' ';
+            b[j] = ' ';
+        }
+    }
+    for (int i = 0; i < 10; i++) {
+        cin >> a[i];
+    }
+    cin >> b;
+    if (funzione(a, b, pos)) {
+        cout << pos;
+    } else { cout << "non presente"; }
+    return 0;
 }
